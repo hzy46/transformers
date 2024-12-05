76a71edff5f127ae0d9a8b3b42aecc2cdd35f98e
Zhiyuan He <zhiyuhe@microsoft.com>
2024-12-05 14:51:49 +0800

    fix

--- src/transformers/models/mistral/modeling_mistral.py
+++ src/transformers/models/mistral/modeling_mistral.py
@@ -44,6 +44,7 @@ from ...utils import (
 )
 from .configuration_mistral import MistralConfig
 
+import gc
 
 if is_flash_attn_2_available():
     from flash_attn import flash_attn_func, flash_attn_varlen_func
@@ -333,6 +334,8 @@ class MistralFlashAttention2(MistralAttention):
         # flash_attn<2.1 generates top-left aligned causal mask, while what is needed here is bottom-right alignement, that was made default for flash_attn>=2.1. This attribute is used to handle this difference. Reference: https://github.com/Dao-AILab/flash-attention/releases/tag/v2.1.0.
         # Beware that with flash_attn<2.1, using q_seqlen != k_seqlen (except for the case q_seqlen == 1) produces a wrong mask (top-left).
         self._flash_attn_uses_top_left_mask = not is_flash_attn_greater_or_equal_2_10()
+        self.debug_attention_query_list = []
+        self.debug_info = {}
 
     def forward(
         self,
@@ -422,8 +425,34 @@ class MistralFlashAttention2(MistralAttention):
         key_states = repeat_kv(key_states, self.num_key_value_groups)
         value_states = repeat_kv(value_states, self.num_key_value_groups)
 
-        print("key_states shape", key_states.shape)
-        print("query_states shape", query_states.shape)
+        # transform 之前
+        # query_states 是 [batch_size, self.num_heads, seq_len, self.head_dim]
+        # key_states 是 [batch_size, self.num_key_value_heads, seq_len, self.head_dim] 如果 mqa 的话 num_key_value_heads 和 self.num_heads 是不一样的
+        # [TBD] confirm sliding window
+        if len(self.debug_attention_query_list) > 0:
+            self.debug_info["attention_query_ret_list"] = []
+            for one_query in self.debug_attention_query_list:
+                start_pos = one_query["start_pos"]
+                window_size = one_query["window_size"]
+                attn_weights = torch.matmul(query_states[..., start_pos:start_pos + window_size, :], key_states[..., :start_pos + window_size, :].transpose(2, 3)) / math.sqrt(self.head_dim)
+                # some code copied from SnapKV
+                mask = torch.full((window_size, window_size), torch.finfo(attn_weights.dtype).min, device=attn_weights.device)
+                mask_cond = torch.arange(mask.size(-1), device=attn_weights.device)
+                mask.masked_fill_(mask_cond < (mask_cond + 1).view(mask.size(-1), 1), 0)
+                mask = mask.to(attn_weights.device)
+                debug_attention_mask = mask[None, None, :, :]
+                attn_weights[:, :, -window_size:, -window_size:] += debug_attention_mask
+                # attention weights: [1, head_num, window_size, seq_len]
+                attn_weights = torch.nn.functional.softmax(attn_weights, dim=-1, dtype=torch.float32).to(query_states.dtype)
+                scores = attn_weights.mean(dim=(1, 2)).detach().cpu().numpy()
+                self.debug_info["attention_query_ret_list"].append({
+                    "scores": scores,
+                    "start_pos": start_pos,
+                    "window_size": window_size,
+                })
+            del attn_weights
+            gc.collect()
+            torch.cuda.empty_cache()
 
         dropout_rate = 0.0 if not self.training else self.attention_dropout
 
