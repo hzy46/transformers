86065d0232ec86cbb9901287b4854cfa752ab815
Zhiyuan He <zhiyuhe@microsoft.com>
2024-12-16 17:29:36 +0800

    fix

--- src/transformers/models/llama/modeling_llama.py
+++ src/transformers/models/llama/modeling_llama.py
@@ -492,7 +492,6 @@ class LlamaFlashAttention2(LlamaAttention):
             # overwrite attention_mask with padding_mask
             attention_mask = kwargs.pop("padding_mask")
 
-        print("exp_setting", self.exp_setting)
         output_attentions = False
 
         bsz, q_len, _ = hidden_states.size()
