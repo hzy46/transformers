99adee3112eca07ce3b30aad8afe693ec53e84c8
Zhiyuan He <zhiyuhe@microsoft.com>
2025-04-08 15:53:41 +0800

    fix

--- src/transformers/models/llama/modeling_llama.py
+++ src/transformers/models/llama/modeling_llama.py
@@ -456,8 +456,7 @@ class LlamaAttention(nn.Module):
             # mask 是 1 的话，取tracking_multiplier * attn_weights，否则，还是原来的
             attn_weights = torch.where(tracking_attention_mask, tracking_multiplier * attn_weights, attn_weights)
             with torch.no_grad():
-                embed()
-                tracking_attn_score_mean = (tracking_attention_mask * attn_weights).sum() / (tracking_attention_mask.sum() * attn_weights.shape[0] * attn_weights.shape[1])
+                tracking_attn_score_mean = (tracking_attention_mask * attn_weights).sum() / (attn_weights.shape[0] * tracking_attention_mask.sum())
                 self.debug_info["tracking_attn_score_mean"] = tracking_attn_score_mean.item()
 
         if len(self.tracking_logit_info_list) > 0:
